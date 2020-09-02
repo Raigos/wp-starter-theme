8015e279f4b4ab3cc9815209931eb14ea129404e
Reigo <rtuulik@gmail.com>
2020-09-02 21:16:59 +0300

    Fix sass, add fonts, convert to sass variables

--- header.php
+++ header.php
@@ -19,6 +19,11 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌺</text></svg>">
 	<?php wp_head(); ?>
+
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
+
+
 </head>
 
 <body <?php body_class(); ?>>
